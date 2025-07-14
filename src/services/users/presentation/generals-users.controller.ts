295fb1dfe122a5db4a65dba325474ed152018ca9
김정호 <jeangho293@naver.com>
2025-07-14 20:07:38 +0900

    [feat] route

---
+++ src/services/users/presentation/generals-users.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('generals/users')
+export class GeneralsUsersController {}
---
+++ src/services/users/presentation/index.ts
@@ -0,0 +1,2 @@
+export * from './admin-users.controller';
+export * from './generals-users.controller';
